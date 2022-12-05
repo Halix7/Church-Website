4b3eec064e0a47b08c2c89db255bc6d06b23c755
Rohit <halixh39@gmail.com>
2022-12-05 14:11:13 +0530

    third commit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const contact = () => {
   return (
     <div className='bg-[rgba(0,0,0,0.4)] backdrop-blur-sm  text-center flex  flex-col py-12 '>
@@ -11,8 +11,8 @@ const contact = () => {
         <p className='text-sm'>There are so many ways to support our mission. <br /> Contact us to find out more about volunteer opportunities, <br /> fundraising events, and ways to get our message to your friends and family.</p>
         <h2 className='font-semibold mt-6'>National Church Of India GD</h2>
         <span className='text-red-500'>+91-9896210674</span>
-        <button className="bg-white  border border-black  mb-2 relative z-10 font-mono text-xl text-black px-5 py-3 mt-10 after:content-['']     after:absolute after:-z-10 after:bg-black after:top-0 after:left-0 after:h-0 after:w-[100%]  hover:text-white hover:after:h-[100%] hover:after:top-auto hover:after:bottom-0 after:transition-all after:ease after:duration-350  "><a href="/prayer">
-          Prayer Request</a></button>
+        <button className="bg-white  border border-black  mb-2 relative z-10 font-mono text-xl text-black px-5 py-3 mt-10 after:content-['']     after:absolute after:-z-10 after:bg-black after:top-0 after:left-0 after:h-0 after:w-[100%]  hover:text-white hover:after:h-[100%] hover:after:top-auto hover:after:bottom-0 after:transition-all after:ease after:duration-350  "><Link to="/prayer">
+          Prayer Request</Link></button>
         </div>
 
     </div>
--- src/components/subcomp/Head.jsx
+++ src/components/subcomp/Head.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import croud from "../../assets/images/croud.jpg"
+import croud from "../../assets/images/croud.jpg";
+import { Link } from 'react-router-dom';
 const Head = () => {
   return (
     <div className="main  bg-cover bg-no-repeat bg-right-bottom" >
@@ -7,8 +8,8 @@ const Head = () => {
         <div className="backdrop-blur-[3px] grid place-items-start text-white font-bold text-lg py-24  px-10 md:p-36 md:text-3xl">
         <span className="text-red-500 bg-white px-2 mb-3 font-serif ">Welcome</span>
           <h1 className="text-3xl font-serif ">Let's <br /> make our <br /> community a <br /> better place!</h1>
-          <button className="bg-white relative z-10 font-mono text-2xl text-black px-5 py-3 mt-10 after:content-['']     after:absolute after:-z-10 after:bg-black after:top-0 after:left-0 after:h-0 after:w-[100%]  hover:text-white hover:after:h-[100%] hover:after:top-auto hover:after:bottom-0 after:transition-all after:ease after:duration-350  "><a href="/contact">
-          GET INVOLVED </a> </button>
+          <button className="bg-white relative z-10 font-mono text-2xl text-black px-5 py-3 mt-10 after:content-['']     after:absolute after:-z-10 after:bg-black after:top-0 after:left-0 after:h-0 after:w-[100%]  hover:text-white hover:after:h-[100%] hover:after:top-auto hover:after:bottom-0 after:transition-all after:ease after:duration-350  "><Link to="/contact">
+          GET INVOLVED </Link> </button>
         </div>
       </div>
   )
