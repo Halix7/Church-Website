d974882c12d32ecca3b3a284ca4c4e371c516610
Rohit <halixh39@gmail.com>
2023-06-14 21:25:36 +0530

    removed branches

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact  path="/about" element={<Contact/>}/>
-        <Route exact  path="/branches" element={<Branches/>}/>
+        <Route exact  path="/branches" element={<Links/>}/>
         <Route exact  path="/links" element={<Links/>}/>
         <Route exact  path="/prayerpartner" element={<PrayerPartner/>}/>
         <Route exact  path="/prayer" element={<Prayer/>}/>
